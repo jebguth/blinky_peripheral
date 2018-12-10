b942b0d7eafac812c64681fd16e238020b4ed62a
Joakim Guth <guth89@gmail.com>
2018-12-10 19:48:23 +0100

    Initial

---
+++ pca10056/s140/ses/Output/ble_app_blinky_pca10056_s140 Release/Obj/utf.d
@@ -0,0 +1,6 @@
+Output/ble_app_blinky_pca10056_s140\ Release/Obj/utf.o: \
+ C:\nordic_semi\SDK\external\utf_converter\utf.c \
+ C:\nordic_semi\SDK\external\utf_converter\utf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.12/include/stddef.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.12/include/__crossworks.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.12/include/stdint.h
